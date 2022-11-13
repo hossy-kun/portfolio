2d85284d93f061f10d1d3fe9edb0a6d5911c1eb4
Kosuke Hoshi <kousuke.1120.hoshi@gmail.com>
2022-11-13 20:13:18 +0900

    add: append viewport meta

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,20 @@
 import '../styles/globals.scss';
 import 'ress';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+};
+
+export default App;
